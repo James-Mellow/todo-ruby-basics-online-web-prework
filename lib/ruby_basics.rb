a260aadac6affc4e85148f5ad1f0d16a17abf6e9
Matthew Muldrow <jmatthewm92@gmail.com>
2019-02-27 09:29:23 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  variable=value  
 end
 
-def argue
-  
+def argue(argue)
+
 end
 
 def greeting
-
+ puts greeting(greeting, name) 
 end
 
 def return_a_value
-
+expect(return_a_value).to eq("Nice")
 end
 
 def last_evaluated_value
-
+expect(last_evaluated_value).to eq("expert")
 end
 
 def pizza_party
-
+  puts (pizza_party)
+expect(pizza_party).to eq("cheese")
 end
